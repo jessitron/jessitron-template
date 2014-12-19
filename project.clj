a959d39d8d28d4055b6013e7001035c6e5dbade0
Jessica Kerr <jessitron@gmail.com>
2014-12-18 22:39:58 -0600

    Removing SNAPSHOT makes 'lein install' work

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jessitron/lein-template "0.1.0-SNAPSHOT"
+(defproject jessitron/lein-template "0.1.0"
   :description "my favorite template, because it comes with test.check"
   :url "http://github.com/jessitron"
   :license {:name "Eclipse Public License"
