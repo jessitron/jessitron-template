0446cf1158e6a3152e2f155badcf678634a45332
Jessica Kerr <jessitron@gmail.com>
2015-03-20 05:49:50 -0500

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jessitron/lein-template "0.1.0"
+(defproject jessitron/lein-template "0.1.1"
   :description "my favorite template, because it comes with test.check"
   :url "http://github.com/jessitron"
   :license {:name "Eclipse Public License"
