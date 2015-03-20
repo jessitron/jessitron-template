0cb43a254c769bea33188290d988feb0655b8bd1
Jessica Kerr <jessitron@gmail.com>
2015-03-20 05:49:13 -0500

    Upgrade schema and test.check

--- src/leiningen/new/jessitron/project.clj
+++ src/leiningen/new/jessitron/project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/test.check "0.6.2"]
-                 [prismatic/schema "0.3.3"]])
+                 [org.clojure/test.check "0.7.0"]
+                 [prismatic/schema "0.3.7"]])
